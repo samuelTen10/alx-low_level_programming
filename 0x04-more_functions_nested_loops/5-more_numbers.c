eca7544324efec44cf24a9459600697a8f0d6a2b
samuel <samuelbegashaw8@gmail.com>
2023-06-23 17:31:02 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,16 @@ void more_numbers(void)
 
 	for (row = 1; row <= 10; row++)
 	{
-		num = count;
-		if (count > 9)
+		for (count = 0; count <= 14; count++)
 		{
-			_putchar(1 + 48);
-			num = count % 10;
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
-		_putchar(num + 48);
+		_puchar('\n');
 	}
-	_puchar('\n');
 }
