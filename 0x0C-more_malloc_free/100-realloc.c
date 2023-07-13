ad89532d199ec071fc9a75d56b33d69f241bcd0f
samuel <samuelbegashaw8@gmail.com>
2023-07-13 14:54:44 -0700

    changes on 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * _realloc - reallocates a memory block
@@ -31,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (p == NULL)
 			return (NULL);
 
-		return (P);
+		return (p);
 	}
 	if (new_size > old_size)
 	{
