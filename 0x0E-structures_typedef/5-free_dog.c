4389f50e65de4d6a60867ca07e0401ddd5cfca8d
samuel <samuelbegashaw8@gmail.com>
2023-07-17 11:17:27 -0700

    changes on 4 and 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free the dog
+ *
+ * @d: the dog
+ *
+ * Return: Always 0.
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
