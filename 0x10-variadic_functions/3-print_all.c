43e2ff7a3a5014095f1f8865a8dc2806feace9c6
samuel <samuelbegashaw8@gmail.com>
2023-07-21 07:36:02 -0700

    changes on 2 and 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -75,7 +75,7 @@ void print_all(const char * const format, ...)
 	int i = 0, j;
 	char *separator = "";
 	va_list ap;
-	token_t token[] = {
+	token_t tokens[] = {
 		{"c", format_char},
 		{"i", format_int},
 		{"f", format_float},
