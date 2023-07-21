246c40a85a81a348ca2f94e35a8fcff0e4a2f9b3
samuel <samuelbegashaw8@gmail.com>
2023-07-21 07:13:26 -0700

    changes on 2 and 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,103 @@
+#include "variadic_functions.h"
+
+/**
+ * format_char - formats a character
+ *
+ * @separator: string separator
+ * @ap: argument
+ *
+ * Return: Always 0.
+*/
+
+void format_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+ * format_int - formats an integer
+ *
+ * @separator: string separator
+ * @ap: argument
+ *
+ * Return: Always 0.
+*/
+
+void format_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+ * format_float - formats a float
+ *
+ * @separator: string separator
+ * @ap: argument
+ *
+ * Return: Always 0.
+*/
+
+void format_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+ * format_string - formats a string
+ *
+ * @separator: string separator
+ * @ap: argument
+ *
+ * Return: Always 0.
+*/
+
+void format_string(char *separator, va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	switch ((int)(!str))
+		case 1:
+			str = "(nil)";
+	
+	printf("%s%s", separator, str);
+}
+
+/**
+ * print_all - prints anything
+ *
+ * @format: the string format
+ *
+ * Return: Always 0.
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *separator = "";
+	va_list ap;
+	token_t token[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (tokens[j].token)
+		{
+			if (format[i] == tokens[j].token[0])
+			{
+				token[j].f(separator, ap);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
