7a25037f6055542d3b472d8b4499cc0579321252
samuel <samuelbegashaw8@gmail.com>
2023-08-01 16:22:39 -0700

    changes on 14

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t idx = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		idx++;
+		i++;
 	}
-	return (idx);
+	return (i);
 }
