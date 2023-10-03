7d6da2afee8b9326a89a35ab6061a9b0898825ea
samuel <samuelbegashaw8@gmail.com>
2023-10-02 19:39:51 -0700

    changes on 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.>
+#include <stdlib.h>
 
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
